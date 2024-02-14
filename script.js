777080a2157e7f97f839addcc0357db848d75ece
Daurkhan <darkhdzhum@mail.ru>
2024-02-15 03:13:08 +0700

    Сделал 16 урок

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-const DomElement = function (selector, height, width, bg, fontSize) {
-    this.selector = selector
-    this.height = height
-    this.width = width
-    this.bg = bg
-    this.fontSize = fontSize
+class First {
+    hello() {
+        console.log("Я родительский метод");
+    }
 }
 
-DomElement.prototype.createElement = function () {
-    let el
-    if (this.selector.startsWith(".")) {
-        el = document.createElement("div")
-        el.classList.add = this.selector;
-
-    } else if (this.selector.startsWith("#")) {
-        el = document.createElement("p")
-        el.id = this.selector;
-
+class Second extends First {
+    constructor() {
+        super()
+        super.hello()
+        this.hello()
+    }
+    hello() {
+        console.log("А я наследуемый метод");
     }
 
-    el.style.height = this.height
-    el.style.width = this.width
-    el.style.background = this.bg
-    el.style.fontSize = this.fontSize
-
-
-    document.body.appendChild(el);
-
-    return (el)
 }
 
-const divEl = new DomElement(".hyi", "50px", "50px", "yellow", "25px")
-const idEl = new DomElement("#pizda", "30px", "30px", "red", "55px")
-
-divEl.createElement();
-idEl.createElement();
+const first = new Second;
+console.log(first);
